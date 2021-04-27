2cbd82af5d2462d4b89deabed10afacc47b52e50
Adexandria <adeolaaderibigbe09@gmail.com>
2021-04-27 08:54:28 +0100

    Added user model

---
+++ Reddit_NewsLetter/Reddit_NewsLetter/Model/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reddit_NewsLetter.Model
+{
+    public class UserModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+    }
+}
