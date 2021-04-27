3adb91d1229a754c486d4745b580c2d2a52eba1f
Adexandria <adeolaaderibigbe09@gmail.com>
2021-04-27 11:20:51 +0100

    Configured the automapper services

--- Reddit_NewsLetter/Reddit_NewsLetter/Startup.cs
+++ Reddit_NewsLetter/Reddit_NewsLetter/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Reddit_NewsLetter.Services;
-
+using System;
 
 namespace Reddit_NewsLetter
 {
@@ -27,6 +27,7 @@ namespace Reddit_NewsLetter
             {
                 opt.UseSqlServer(Configuration.GetConnectionString("Reddit_NewsLetter"));
             });
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
