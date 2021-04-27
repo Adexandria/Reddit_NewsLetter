5068d823747ce62d113830d9c2e3292a1f947f08
Adexandria <adeolaaderibigbe09@gmail.com>
2021-04-27 09:44:51 +0100

    added the view data transfer object for the user model

---
+++ Reddit_NewsLetter/Reddit_NewsLetter/ViewDTO/UserDTO.cs
@@ -0,0 +1,12 @@
+﻿using Reddit_NewsLetter.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reddit_NewsLetter.ViewDTO
+{
+    public class UserDTO : UserModel
+    {
+    }
+}
