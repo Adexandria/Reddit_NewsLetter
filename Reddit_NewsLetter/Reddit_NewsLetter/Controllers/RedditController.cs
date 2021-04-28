41271096dae8017ce9331320355b9eedf0442883
Adexandria <adeolaaderibigbe09@gmail.com>
2021-04-28 18:05:43 +0100

    Added the search reddit api

--- Reddit_NewsLetter/Reddit_NewsLetter/Controllers/RedditController.cs
+++ Reddit_NewsLetter/Reddit_NewsLetter/Controllers/RedditController.cs
@@ -9,6 +9,8 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Text;
 using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Reddit_NewsLetter.RedditModel;
 
 namespace Reddit_NewsLetter.Controllers
 {
@@ -20,25 +22,29 @@ namespace Reddit_NewsLetter.Controllers
         private readonly string client_Secret = Environment.GetEnvironmentVariable("Client_Secret");
         private readonly string urlBase = Environment.GetEnvironmentVariable("AccessUrl");
 
-        [HttpPost]
-        public  ActionResult Index()
+        [HttpPost("search/{name}")]
+        public  ActionResult Post(string name)
         {
             var result = GetAccessCode().Result;
-            return Ok(result);
+            var subreddits = GetAvailableSubreddit(name, result);
+            return Ok(subreddits);
 
+        }
+        private List<string> GetAvailableSubreddit(string name,string result) 
+        {
+            var reddit = new Reddit.RedditClient(appId: client_Id, appSecret: client_Secret, accessToken: result);
+            var subreddits = reddit.SearchSubredditNames(name);
+            var subredditList = subreddits.Select(s => s.Name).ToList();
+            return subredditList;
         }
         private async Task<string> GetAccessCode()
         {
             try
             {
-                var client = new HttpClient();
-                var Id = client_Id + ":" + client_Secret;
-                var base64Credentials = GetEncodedString(Id);
-                String authorizationHeader = "Basic " + base64Credentials;
-                client.DefaultRequestHeaders.Add("Authorization", authorizationHeader);
-                HttpResponseMessage response = await client.PostAsync(urlBase, null);
+                var response = await GetResponse(urlBase);
                 var contenttype = await response.Content.ReadAsStringAsync();
-                return contenttype;
+                var accessCode = JsonConvert.DeserializeObject<ResultModel>(contenttype);
+                return accessCode.AccessCode;
             }
             catch (Exception e)
             {
@@ -46,15 +52,41 @@ namespace Reddit_NewsLetter.Controllers
                 return e.Message;
             }
         }
+        private async Task<string> GetRefreshCode(string token) 
+        {
+            try
+            {
+                var parameter = $"?grant_type=refresh_token&refresh_token={token}";
+                var url = urlBase + parameter;
+                var response = await GetResponse(url);
+                var contenttype = await response.Content.ReadAsStringAsync();
+                return contenttype;
+            }
+            catch(Exception e)
+            {
+                return e.Message;
+            }
+            
+        }
+        private async Task<HttpResponseMessage> GetResponse(string url) 
+        {
+            var client = new HttpClient();
+            var Id = client_Id + ":" + client_Secret;
+            var base64Credentials = GetEncodedString(Id);
+            String authorizationHeader = "Basic " + base64Credentials;
+            client.DefaultRequestHeaders.Add("Authorization", authorizationHeader);
+            HttpResponseMessage response = await client.PostAsync(url, null);
+            return response;
+        }
         private string GetEncodedString(string id)
         {
             byte[] toEncodeAsBytes
 
-              = System.Text.ASCIIEncoding.ASCII.GetBytes(id);
+              = ASCIIEncoding.ASCII.GetBytes(id);
 
             string returnValue
 
-                  = System.Convert.ToBase64String(toEncodeAsBytes);
+                  = Convert.ToBase64String(toEncodeAsBytes);
 
             return returnValue;
         }
