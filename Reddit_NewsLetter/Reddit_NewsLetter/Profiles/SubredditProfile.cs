cd728ed7707ee6369f259fcf9e8273eb80b06caf
Adexandria <adeolaaderibigbe09@gmail.com>
2021-04-29 07:32:41 +0100

    Subreddit profile

---
+++ Reddit_NewsLetter/Reddit_NewsLetter/Profiles/SubredditProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Reddit_NewsLetter.Model;
+using Reddit_NewsLetter.ViewDTO;
+
+
+namespace Reddit_NewsLetter.Profiles
+{
+    public class SubredditProfile :Profile
+    {
+        public SubredditProfile()
+        {
+            CreateMap<SubredditModel, SubredditDTO>()
+                .ForMember(s => s.Subreddit, m => m.MapFrom(s => s.Subreddit));
+
+            CreateMap<SubredditCreate, SubredditModel>()
+               .ForMember(s => s.Subreddit, m => m.MapFrom(s => s.Subreddit));
+        }
+    }
+}
